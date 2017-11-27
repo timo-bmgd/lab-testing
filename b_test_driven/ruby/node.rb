00c6dd51ec0f5d0cba9a069c9231e3d42ffdf9de
B Kleinen <kleinen@infrastructure.de>
2017-11-27 20:57:47 +0100

    fixed ruby style issues

--- b_test_driven/ruby/node.rb
+++ b_test_driven/ruby/node.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
+# Node and Head of a LinkedList
 class Node
-	attr_accessor :data, :next_node
-	def initialize(data,next_node = nil)
-		@data = data
-		@next_node = next_node
-	end
-	def self.string_to_list(s)
-		nodes = s.split(",")
-		nodes.reverse.inject(nil){|list, element| Node.new(element,list)}
-	end
+  attr_accessor :data, :next_node
 
-	def to_s
-		unless next_node
-		  data.to_s 
-		else
-		  data.to_s + ", " + next_node.to_s
-		end
+  def initialize(data, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end
+
+  def self.string_to_list(s)
+    nodes = s.split(',')
+    nodes.reverse.inject(nil) { |list, element| Node.new(element, list) }
+  end
+
+  def to_s
+    if next_node.nil?
+      data.to_s
+    else
+      data.to_s + ', ' + next_node.to_s
     end
+  end
 
-    def delete(data)
-    	# this is a too simple implementation to satisfy the first deletion example.
-    	return next_node
-    end
-
+  def delete(data)
+    # this is a too simple implementation to satisfy the first deletion example.
+    next_node
+  end
 end
--- b_test_driven/ruby/node_spec.rb
+++ b_test_driven/ruby/node_spec.rb
@@ -1,28 +1,27 @@
 require_relative 'node.rb'
 describe Node do
-	describe 'creation' do
-		it 'has a convenience constructor' do
-			node = Node.string_to_list('a,b,c')
-			expect(node.to_s).to eq("a, b, c")
-		end
-
-	end
-	describe "to_s" do
-		it "is the element for a list with one element" do
-			expect(Node.new("X").to_s).to eq("X")
-		end
-		it "concatenates multiple elements with a ," do
-			expect(Node.new("A",Node.new("B")).to_s).to eq("A, B")
-		end
-	end
-	describe "deletion" do
-		context "from a three element list" do
-			let(:list){Node.new("a",Node.new("b",Node.new("c")))}
-			it "deletes first element" do
-				expect(list.delete("a").to_s).to eq("b, c")
-			end
-			it "deletes element in the middle"
-			it "deletes last element"
-		end
-	end
+  describe 'creation' do
+    it 'has a convenience constructor' do
+      node = Node.string_to_list('a,b,c')
+      expect(node.to_s).to eq('a, b, c')
+    end
+  end
+  describe 'to_s' do
+    it 'is the element for a list with one element' do
+      expect(Node.new('X').to_s).to eq('X')
+    end
+    it 'concatenates multiple elements with a ,' do
+      expect(Node.new('A', Node.new('B')).to_s).to eq('A, B')
+    end
+  end
+  describe 'deletion' do
+    context 'from a three element list' do
+      let(:list) { Node.new('a', Node.new('b', Node.new('c'))) }
+      it 'deletes first element' do
+        expect(list.delete('a').to_s).to eq('b, c')
+      end
+      it 'deletes element in the middle'
+      it 'deletes last element'
+    end
+  end
 end
